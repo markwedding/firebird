4813f9a152b1fcf0cd202a9065b0047feb3f9a2e
markwedding <markwedding@protonmail.com>
2022-05-21 11:10:54 -0400

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+  );
+};
+
+export default App;
